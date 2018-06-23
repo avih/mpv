4080d9369c0e7cca593748bf3485122bf86e2b33
Avi Halachmi (:avih) <avihpit@yahoo.com>
2018-06-23 03:48:34 +0300

    osc: add script-opt "mousewheel" (enable by default)
    When enabled (default) the OSC grabs all mouse-wheel events and handles
    them internally, e.g. for volume while hovering the volume icon.
    
    However, this prevents other, possibly user-defined mouse wheel
    handlers, like for seek - while hovering the OSC.
    
    Disabling this option allow the default handlers to work also over the
    OSC - which disables the OSC handling, e.g. for volume.

--- player/lua/osc.lua
+++ player/lua/osc.lua
@@ -49,6 +49,9 @@ local user_opts = {
     windowcontrols_alignment = "right", -- which side to show window controls on
     greenandgrumpy = false,     -- disable santa hat
     livemarkers = true,         -- update seekbar chapter markers on duration change
+
+
+    mousewheel = true,           -- OSC handles (e.g.volume) and prevents default
 }
 
 -- read options from config and command-line
@@ -2735,7 +2738,7 @@ mp.set_key_bindings({
 do_enable_keybindings()
 
 --mouse input bindings
-mp.set_key_bindings({
+local mi = {
     {"mbtn_left",           function(e) process_event("mbtn_left", "up") end,
                             function(e) process_event("mbtn_left", "down")  end},
     {"shift+mbtn_left",     function(e) process_event("shift+mbtn_left", "up") end,
@@ -2745,12 +2748,15 @@ mp.set_key_bindings({
     -- alias to shift_mbtn_left for single-handed mouse use
     {"mbtn_mid",            function(e) process_event("shift+mbtn_left", "up") end,
                             function(e) process_event("shift+mbtn_left", "down")  end},
-    {"wheel_up",            function(e) process_event("wheel_up", "press") end},
-    {"wheel_down",          function(e) process_event("wheel_down", "press") end},
     {"mbtn_left_dbl",       "ignore"},
     {"shift+mbtn_left_dbl", "ignore"},
     {"mbtn_right_dbl",      "ignore"},
-}, "input", "force")
+}
+if user_opts.mousewheel then
+    mi[#mi+1] = {"wheel_up",   function(e) process_event("wheel_up", "press") end}
+    mi[#mi+1] = {"wheel_down", function(e) process_event("wheel_down", "press") end}
+end
+mp.set_key_bindings(mi, "input", "force")
 mp.enable_key_bindings("input")
 
 mp.set_key_bindings({
